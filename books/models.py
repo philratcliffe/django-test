27c8e0b61a2fc99fe790dc165bc9562098ede2f6
Phil Ratcliffe <phil@philratcliffe.co.uk>
2019-05-25 15:42:40 +0100

    Intial commit

---
+++ books/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.title
---
+++ person/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+class Address(models.Model):
+    line = models.CharField(max_length=100)
+
+    def  __str__(self):
+        return self.line
+
+class Person(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.ForeignKey(Address, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+
